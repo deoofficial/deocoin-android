0a2c0dfa043f04c7946786d800ee214fd2cc3f2d
deoofficial <kenayaraffani@gmail.com>
2019-11-02 03:03:34 +0700

    initial commit

---
+++ src/js/trezor-url.js
@@ -0,0 +1,2 @@
+window.TREZOR_CHROME_URL = './bower_components/trezor-connect/chrome/wrapper.html';
+// window.__TREZOR_CONNECT_SRC = 'https://trezor-connect.deocoin.org/';
---
+++ src/js/version.js
@@ -0,0 +1,2 @@
+window.version="2.7.1";
+window.commitHash="c85b838";
